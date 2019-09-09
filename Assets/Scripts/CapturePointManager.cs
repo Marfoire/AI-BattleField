cca8fa62a6b6b25fe5bfa07729a5c70f98142246
Marfoire <44646509+Marfoire@users.noreply.github.com>
2019-09-08 20:55:00 -0400

    Initial commit

---
+++ Assets/Scripts/CapturePointManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CapturePointManager : MonoBehaviour
+{
+    public List<GameObject> charactersOnCapturePoint = new List<GameObject>();
+
+    public float blueScoreValue;
+    public float redScoreValue;
+
+    public GameObject blueScoreText;
+    public GameObject redScoreText;
+
+    private void Awake()
+    {
+        blueScoreValue = 0;
+        redScoreValue = 0;
+
+        blueScoreText.GetComponent<Text>().text = blueScoreValue.ToString();
+        redScoreText.GetComponent<Text>().text = redScoreValue.ToString();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "BlueTeam" || other.gameObject.tag == "RedTeam")
+        {
+            charactersOnCapturePoint.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "BlueTeam" || other.gameObject.tag == "RedTeam")
+        {
+            charactersOnCapturePoint.Remove(other.gameObject);
+        }
+    }
+
+    private void AddToScoreValues()
+    {
+        if (Time.frameCount % 60 == 0)
+        {
+            foreach (GameObject c in charactersOnCapturePoint)
+            {
+                if (c.tag == "BlueTeam")
+                {
+                    blueScoreValue++;
+                    blueScoreText.GetComponent<Text>().text = blueScoreValue.ToString();
+                }
+                else if (c.tag == "RedTeam")
+                {
+                    redScoreValue++;
+                    redScoreText.GetComponent<Text>().text = redScoreValue.ToString();
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        AddToScoreValues();
+    }
+
+}

a12b029cf84a92afc21be51a8839cd4ba8b4adce
Marfoire <44646509+Marfoire@users.noreply.github.com>
2019-09-10 11:57:17 -0400

    Updated unity and did some capture point things

--- Assets/Scripts/CapturePointManager.cs
+++ Assets/Scripts/CapturePointManager.cs
@@ -7,11 +7,17 @@ public class CapturePointManager : MonoBehaviour
 {
     public List<GameObject> charactersOnCapturePoint = new List<GameObject>();
 
+    public Gradient g;
     public float blueScoreValue;
     public float redScoreValue;
 
     public GameObject blueScoreText;
     public GameObject redScoreText;
+
+    public GameObject scoreOnePointParticle;
+    private GameObject particleToSpawn;
+
+    private LineRenderer outlineParticle;
 
     private void Awake()
     {
@@ -20,6 +26,8 @@ public class CapturePointManager : MonoBehaviour
 
         blueScoreText.GetComponent<Text>().text = blueScoreValue.ToString();
         redScoreText.GetComponent<Text>().text = redScoreValue.ToString();
+
+        outlineParticle = transform.GetComponentInChildren<LineRenderer>();
     }
 
 
@@ -43,21 +51,50 @@ public class CapturePointManager : MonoBehaviour
     {
         if (Time.frameCount % 60 == 0)
         {
-            foreach (GameObject c in charactersOnCapturePoint)
+            foreach (GameObject character in charactersOnCapturePoint)
             {
-                if (c.tag == "BlueTeam")
-                {
+                if (character.tag == "BlueTeam")
+                {                  
                     blueScoreValue++;
+                    particleToSpawn = Instantiate(scoreOnePointParticle, character.transform.position, Quaternion.identity);
+                    particleToSpawn.GetComponent<PointScoreParticleBehaviour>().onBlueTeam = true;
                     blueScoreText.GetComponent<Text>().text = blueScoreValue.ToString();
                 }
-                else if (c.tag == "RedTeam")
+                else
                 {
                     redScoreValue++;
+                    particleToSpawn = Instantiate(scoreOnePointParticle, character.transform.position, Quaternion.identity);
+                    particleToSpawn.GetComponent<PointScoreParticleBehaviour>().onBlueTeam = false;
                     redScoreText.GetComponent<Text>().text = redScoreValue.ToString();
                 }
             }
         }
     }
+
+    void UpdateLineRendererColour()
+    {
+        int redCharactersOnPoint = 0;
+        int blueCharactersOnPoint = 0;
+        foreach (GameObject character in charactersOnCapturePoint)
+        {
+            if(character.tag == "BlueTeam")
+            {
+                blueCharactersOnPoint++;
+            }
+            else
+            {
+                redCharactersOnPoint++;
+            }
+        }
+
+        float avg = 0.5f;
+        avg -= redCharactersOnPoint / 20f;
+        avg += blueCharactersOnPoint / 20f;
+        var c = g.Evaluate(Mathf.Clamp01(avg));
+        outlineParticle.startColor = new Color(outlineParticle.startColor.r + redCharactersOnPoint * (20f / byte.MaxValue), outlineParticle.startColor.g, outlineParticle.startColor.b + blueCharactersOnPoint * (20f / byte.MaxValue));
+    }
+
+
 
     private void Update()
     {
---
+++ Assets/Scripts/PointScoreParticleBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointScoreParticleBehaviour : MonoBehaviour
+{
+    public Color blueColour;
+    public Color redColour;
+
+    public bool onBlueTeam;
+
+    public float risingRate;
+
+    public float fadeRate;
+
+
+    void GraduallyRiseAndFade()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + risingRate * Time.deltaTime, transform.position.z);
+        if (onBlueTeam)
+        {           
+            blueColour = new Color(blueColour.r, blueColour.g, blueColour.b, blueColour.a - fadeRate * Time.deltaTime);
+            GetComponent<TextMesh>().color = blueColour;
+        }
+        else
+        {
+            redColour = new Color(redColour.r, redColour.g, redColour.b, redColour.a - fadeRate * Time.deltaTime);
+            GetComponent<TextMesh>().color = redColour;
+        }
+        if(GetComponent<TextMesh>().color.a <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        GraduallyRiseAndFade();
+    }
+}

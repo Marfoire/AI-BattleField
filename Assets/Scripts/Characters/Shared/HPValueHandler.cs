f654e21783a4004f77790a12117f70ce13b29bf3
Marfoire <44646509+Marfoire@users.noreply.github.com>
2019-09-16 22:00:18 -0400

    Added particle effects for healing and hurting

--- Assets/Scripts/Characters/Shared/HPValueHandler.cs
+++ Assets/Scripts/Characters/Shared/HPValueHandler.cs
@@ -7,10 +7,14 @@ public class HPValueHandler : MonoBehaviour
 {
 
     public float myHP;
+    public float maxHP;
+    public GameObject healParticles;
+    public GameObject hurtParticles;
 
     void Awake()
     {
         myHP = GetComponent<Blackboard>().GetFloatVar("hpValue").Value;
+        maxHP = myHP;
     }
 
     void UpdateHPVar()
@@ -21,12 +25,14 @@ public class HPValueHandler : MonoBehaviour
     public void TakeDamage()
     {
         myHP--;
+        Instantiate(hurtParticles, transform);
         UpdateHPVar();
     }
 
     public void HealHp()
     {
         myHP++;
+        Instantiate(healParticles, transform);
         UpdateHPVar();
     }
 

36abb7a2a59161d993d37f5315c8b7b8381e2394
Marfoire <44646509+Marfoire@users.noreply.github.com>
2019-09-16 22:01:03 -0400

    Particle prefab playing script

---
+++ Assets/Scripts/Environment/PlayParticleSystemThenDelete.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticleSystemThenDelete : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        GetComponent<ParticleSystem>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GetComponent<ParticleSystem>().isPlaying == false)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

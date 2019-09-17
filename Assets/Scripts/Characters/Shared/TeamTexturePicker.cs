14284dc7a9ea925682e0fb33f1f640a4c25b2c4e
Marfoire <44646509+Marfoire@users.noreply.github.com>
2019-09-16 21:59:35 -0400

    Made textures for Cleric and Knights and turned them to assignable materials

---
+++ Assets/Scripts/Characters/Shared/TeamTexturePicker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamTexturePicker : MonoBehaviour
+{
+    public Material blueTexture;
+    public Material redTexture;
+
+    private void Start()
+    {
+        if (transform.parent.tag == "BlueTeam")
+        {
+            GetComponent<MeshRenderer>().material = blueTexture;
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().material = redTexture;
+        }
+    }
+
+}

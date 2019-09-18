1a800a066cb7bd3c97de62baf0b17058e9c1e138
Marfoire <44646509+Marfoire@users.noreply.github.com>
2019-09-18 01:03:45 -0400

    borrowed a camera controller

---
+++ Assets/Scripts/Environment/CameraController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+
+    //controller script, courtesy of Phill
+
+
+    [SerializeField]
+    private float speed = 2f;
+
+    [SerializeField]
+    private float acceleration = 4f;
+
+    [SerializeField]
+    private float sensitivity = 3f;
+
+    [SerializeField]
+    private bool invert = false;
+
+    private Vector3 velocity;
+
+    private void Update()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        Movement();
+        LookingAround();
+    }
+
+    private void Movement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = 0f;
+        float lateral = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            vertical--;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            vertical++;
+        }
+
+        Vector3 direction = new Vector3(horizontal, vertical, lateral).normalized;
+        direction = transform.TransformDirection(direction);
+
+        float multiplier = 1f;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            multiplier = 1.65f;
+        }
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            multiplier = 0.5f;
+        }
+
+        velocity = Vector3.MoveTowards(velocity, direction, Time.smoothDeltaTime * acceleration);
+        transform.position += velocity * Time.smoothDeltaTime * speed * multiplier;
+    }
+
+    private void LookingAround()
+    {
+        float sourceRatio = 7.2f / (4090909090909f / 250000000000f); //for making a repeating 16.3636
+        Vector2 mouse = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        mouse *= sourceRatio * sensitivity;
+
+        if (invert)
+        {
+            mouse.y *= -1;
+        }
+
+        transform.eulerAngles += new Vector3(mouse.y, mouse.x, 0);
+    }
+}

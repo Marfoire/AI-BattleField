b8674af1562bb05f3f250d904c26c669b10ed307
Marfoire <44646509+Marfoire@users.noreply.github.com>
2019-09-26 23:42:49 -0400

    Implemented arrow prefab and script, asset pending

---
+++ Assets/Scripts/Attacks/ArrowBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowBehaviour : MonoBehaviour
+{
+
+    public string myTeamTag;
+
+    public void GiveMeMyTagToIgnore(string tag)
+    {
+        myTeamTag = tag;
+        GetComponent<Collider>().enabled = true;
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().rotation = Quaternion.LookRotation(GetComponent<Rigidbody>().velocity.normalized);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag != myTeamTag && (other.isTrigger == false || other.tag == tag))
+        {
+            if(other.tag == "BlueTeam" || other.tag == "RedTeam")
+            {
+                other.GetComponent<HPValueHandler>().TakeDamage();
+            }
+            Destroy(gameObject);
+        }
+    }
+
+}

7045ccaf2a6b2c093d2f7b5f4a06c6376fe3613b
Marfoire <44646509+Marfoire@users.noreply.github.com>
2019-09-26 23:43:16 -0400

    Created mage cast state

---
+++ Assets/Scripts/Characters/Mage/MageCastState.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using BehaviourMachine;
+
+public class MageCastState : StateBehaviour
+{
+	// Called when the state is enabled
+	void OnEnable () {
+		Debug.Log("Started *State*");
+	}
+ 
+	// Called when the state is disabled
+	void OnDisable () {
+		Debug.Log("Stopped *State*");
+	}
+	
+	
+}
+
+
